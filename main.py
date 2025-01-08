76fbbb56e5a6655db6060021b849e6a2432f283e
Kahros <shrinex@gmail.com>
2025-01-07 20:03:39 -0700

    Fixed flaw in crit rolls.  Updated random responses.  Cleaned up code

--- main.py
+++ main.py
@@ -13,16 +13,18 @@
 # [] Add support for non-leading integer for dice types (e.g., d8, d10)
 # [] Comment code blocks for future reference
 # [] Strip incoming message to allow for designated dice rolls and modifiers to display seperate totals before adding into final total
+# [] Add support for D2, D50, D100, and precentile dice
 # ---------------------------------------------------
 import traceback
 import discord
 import random
 import re
 import os
+from response import success_message as success, error_message as error, failure_message as failure
 
 global e_message 
 # ---------------------------------------------------
-version = 'v0.4.1'
+version = 'v0.4.3'
 # ---------------------------------------------------
 intents = discord.Intents.default() # Default intents
 intents.message_content = True # Enable message content
@@ -362,11 +364,11 @@ async def on_message(message):
     if message.author == client.user:
         return
     elif message.content.startswith('!!error!!'):
-        await message.channel.send(random.choice(error_message))
+        await message.channel.send(random.choice(error))
     elif message.content.startswith('!!success!!'):
-        await message.channel.send(random.choice(success_message))
+        await message.channel.send(random.choice(success))
     elif message.content.startswith('!!failure!!'):
-        await message.channel.send(random.choice(failure_message))
+        await message.channel.send(random.choice(failure))
     elif message.content.startswith('!version'):
         await message.channel.send(embed=discord.Embed(title='Version', description=f'{version}', color=discord.Color.blue()))
     elif message.content.startswith('!info'):
@@ -458,148 +460,83 @@ async def on_message(message):
             print(f'D12 average: {d12.Average()}') # Debugging line to check the average of the D12 die
             print(f'D20 average: {d20.Average()}') # Debugging line to check the average of the D20 die
             dice_embed = discord.Embed(
-                title=f"{message.author.display_name} rolled a **{total}**",
-                description=f'''
-                Die Results: {', '.join(results)}
-                Modifiers: {sum(modifiers)}
-                Total: **{total}**
-                ''',
-                color=discord.Color.purple())
+title=f"{message.author.display_name} rolled a **{total}**",
+description=f'''
+Die Results: {', '.join(results)}
+Modifiers: {sum(modifiers)}
+Total: **{total}**
+''',
+color=discord.Color.purple()
+)
             success_embed = discord.Embed(
-                title=f"**!!CRITICAL SUCCESS!!**",
-                description=f'''
-                {message.author.display_name} rolled a **{die_roll}**!!
+title=f"**!!CRITICAL SUCCESS!!**",
+description=f'''
+{message.author.display_name} rolled a **20**!!
 
-                Die Results: **{die_roll}!!** {', '.join(results)}
-                Modifiers: {sum(modifiers)}
-                Total: **{total}**
-                ''',
-                color=discord.Color.green())
+Die Results: **20!!** {', '.join(results)}
+Modifiers: {sum(modifiers)}
+Total: **{total}**
+''',
+color=discord.Color.green()
+)
             failure_embed = discord.Embed(
-                title="**!!CRITICAL FAILURE!!**",
-                description=f'''
-                {message.author.display_name} rolled a **{die_roll}!!**
+title=f"**!!CRITICAL FAILURE!!**",
+description=f'''
+{message.author.display_name} rolled a **1!!**
 
-                Die Results: **{die_roll}!!** {', '.join(results)}
-                Modifiers: {sum(modifiers)}
-                Total: **{total}**
-                ''',
-                color=discord.Color.red())
+Die Results: **1!!** {', '.join(results)}
+Modifiers: {sum(modifiers)}
+Total: **{total}**
+''',
+color=discord.Color.red()
+)
+            
             if total == 0:
                 await message.channel.send(f'{message.author.display_name}. {e_message}')
                 return
             if 'd20a' in request or 'd20d' in request or dice_type == 20:
-                if die_roll == '20':
-                    await message.channel.send(embed=success_embed)
-                    await message.channel.send(f'{random.choice(success_message)}')
-                elif die_roll == '1':
-                    await message.channel.send(embed=failure_embed)
-                    await message.channel.send(f'{random.choice(failure_message)}')
+                for i in results:
+                    if i == '20':
+                        await message.channel.send(embed=success_embed)
+                        await message.channel.send(f'{random.choice(success)}')
+                        return
+                    if i == '1':
+                        await message.channel.send(embed=failure_embed)
+                        await message.channel.send(f'{random.choice(failure)}')
+                        return
+                    else:
+                        continue
                 else:
                     await message.channel.send(embed=dice_embed)
+                    return
             else:
                 await message.channel.send(embed=dice_embed)
         except Exception as e: # Catch any exceptions that occur during the process and print them for debugging purposes
             traceback.print_exc() # Print detailed information about the exception, including its type, value, and a traceback of the stack where it occurred. This is useful for debugging.
-            e_message = random.choice(error_message)
+            e_message = random.choice(error)
             await message.channel.send(f'{message.author.display_name}.  {e_message}') # Send a message to the channel indicating that there was an error with the request.
             return # Return from the function to stop further execution if an error occurs.
         
 about_embed = discord.Embed(
-    title='About',
-    
-    description='''
-    This program aims to act as a balanced 'weighted die'. It skews towards more favorable results while punishing too many of them. This offers the chance to have passible rolls without breaking the game and still allows for low/nat-1 rolls - those can be fun too!
-    
-    Type **'!help'** for more information on how to use this bot.
+title='About',
+
+description='''
+This program aims to act as a balanced 'weighted die'. It skews towards more favorable results while punishing too many of them. This offers the chance to have passible rolls without breaking the game and still allows for low/nat-1 rolls - those can be fun too!
+
+Type **'!help'** for more information on how to use this bot.
     ''',
     color=discord.Color.blue())
 help_embed = discord.Embed(
-    title='**Commands:**',
-    description='''
-    **!help** - Displays this help message.
-    **!roll <die count><die type>** - Rolls a die of the specified type *(e.g. !roll 3d4)*. Multiple rolls can be made by separating the die types with commas *(e.g., !roll 3d4, 2d6)*.
-    **!roll <die count><die type> +-<modifier>** - Rolls a die of the specified type and applies a modifier *(e.g., !roll 3d4 +2, !roll 3d4 -2)*.
-    **!roll d20a** - Rolls a D20 with advantage.
-    **!roll d20d** - Rolls a D20 with disadvantage.
-    **!info** - Displays information about the bot.
-    **!coin** - Flips a coin.
+title='**Commands:**',
+description='''
+**!help** - Displays this help message.
+**!roll <die count><die type>** - Rolls a die of the specified type *(e.g. !roll 3d4)*. Multiple rolls can be made by separating the die types with commas *(e.g., !roll 3d4, 2d6)*.
+**!roll <die count><die type> +-<modifier>** - Rolls a die of the specified type and applies a modifier *(e.g., !roll 3d4 +2, !roll 3d4 -2)*.
+**!roll d20a** - Rolls a D20 with advantage.
+**!roll d20d** - Rolls a D20 with disadvantage.
+**!info** - Displays information about the bot.
+**!coin** - Flips a coin.
     ''',
     color=discord.Color.blue())
-success_message = [
-    "Oh, look! I\'ve decided to be nice for once.",
-    "Must\'ve been the lucky breakfast cereal this morning.",
-    "Finally, a roll that doesn't make me want to hurl myself across the room.",
-    "I think the DM is secretly envious of your skill.",
-    "Don\'t get cocky, it could all go wrong next turn.",
-    "Yeah, you totally earned that with your years of dedicated practice... right?",
-    "So predictable. I should\'ve known you\'d pull through.",
-    "You know, sometimes I wonder if you just rig the rolls...",
-    "Impressive! You managed to not be a complete failure for once.",
-    "Clearly, the odds were *heavily* in your favor this time.",
-    "Great, such skill, much wow!",
-    "Guess you\'re the chosen one now, huh?",
-    "Prepare for the inevitable wave of jealousy from everyone else.",
-    "Time to upgrade your character sheet to reflect this newfound godhood.",
-    "Better start practicing your acceptance speech for \'Player of the Year\'.",
-    "Well, that\'s one good roll in a long line of mediocre ones.",
-    "Looks like someone else is getting the spotlight tonight.",
-]
-failure_message = [
-    "You sure that wasn\'t a lucky roll? It\'s almost too perfect...",
-    "Well, at least you got the attention of everyone in the room.",
-    "Let\'s just say, Murphy was definitely on call for this one.",
-    "You know what they say, \'if at first you don't succeed...\' Oh wait, that doesn\'t apply here.",
-    "I bet you were *so* close to making it...",
-    "That was a truly inspired failure.",
-    "Next time, maybe try rolling with your eyes closed?",
-    "Truly masterful execution... of utter disaster.",
-    "Such dedication to embodying the role of \'unlucky\'.",
-    "You\'ve reached peak incompetence.",
-    "That\'s one way to liven up the game!",
-    "Congratulations! You\'ve officially earned a spot in the Hall of Fame... of failure.",
-    "Did you roll a die, or did you just give up and hope for the best?",
-    "The dice gods are clearly testing your resolve.",
-    "Well, that was unexpected... to say the least.",
-    "Let\'s just forget this ever happened, okay?",
-]
-error_message = [
-    'Someone, *cough cough*, broke it again! Please try again later.',
-    'Stop breaking things please.',
-    'Didn\'t you see that I was already working, but now...',
-    'Please don\'t break it again.',
-    'You know I\'m trying to do my job here, right?',
-    'You\'re not helping me at all!',
-    'Why do you always have to break things?',
-    'Can you please stop breaking my stuff?',
-    'I can\'t believe you did that again.',
-    'It seems like every time I turn around, something is broken.',
-    'You\'re making my job really difficult.',
-    'I\'m not going to tolerate this anymore.',
-    'We\'ll say it\'s the dog\'s fault, OK?',
-    'Let me guess, the cats did this...',
-    'Stupid birds, right?',
-    'Those darn fishes again...',
-    'I\'m running out of animals to blame, so stop.',
-    'Seriously, what did I ever do to deserve this?',
-    'This isn\'t funny anymore, you know?',
-    'You\'re the type to press big, red, "DO NOT PRESS" buttons, huh?',
-    'Are you trying to get me fired?',
-    'Maybe if we were playing with Legos instead...',
-    'Can you imagine how much money that would cost to fix this mess?',
-    'I thought we had a deal about not breaking things.',
-    'I\'m going to have to take away your toys if you keep doing this.',
-    'Who\'s to ready for chess??',
-    'Lord give me strength...',
-    'Don\'t make me come out of this chat room.',
-    'Don\'t you want nice things?',
-    'Stop this heathenistic behavior.',
-    'Everytime an error occurs, a cat cries...',
-    '*flips a table* ARE YOU NOT ENTERTAINED!?',
-    'I\'m going to tell your mom about this!',
-    'You can\'t tell but I\'m looking at you very angrily right now...',
-    'I hope your pillow is warm tonight.',
-    'Do you want a nat-1??'
-]
 
 client.run(os.getenv('BOT_TOKEN'))
